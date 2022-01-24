9e1d727d25a6b35ec1f966986390316ea3aaa291
zacharyjbaldwin <zacharyjbaldwin@gmail.com>
2022-01-24 16:38:24 -0600

    Added some frontend and set up handlebars

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Node modules
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const hbs = require('hbs');
 const MongoClient = require('mongodb').MongoClient;
 const morgan = require('morgan');
+const path = require('path');
 // const session = require('express-session');
 const session = require('cookie-session');
 
@@ -10,8 +12,15 @@ const session = require('cookie-session');
 const config = require('./config.json');
 const routes = require('./routes/routes');
 
+// Handlebars setup
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
 // Express setup
 const app = express();
+app.set('view engine', hbs);
+
+// Express middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(session({
@@ -20,12 +29,13 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: 'auto' }
 }));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules'))); // This line alllows us to reference things like bootstrap
 
 // Routing
 app.use('/', routes);
 
 app.use((req, res) => {
-    res.send('That\'s an error.');
+    res.send('That\'s an problem.');
 });
 
 app.listen(config.port, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -4,27 +4,55 @@ router.get('/', (req, res) => {
     res.send('Welcome!');
 });
 
-router.get('/:user/addhive', (req, res) => {
-    res.send(`Adding hive to ${req.params.user}`)
+router.get('/login', (req, res) => {
+    let message;
+    if (req.query.reason) {
+        switch (req.query.reason) {
+            case 'noSession':
+                message = 'You must be logged in to do that!'
+                break;
+        }
+    }
+
+    res.render('login.hbs', {
+        message: message
+    });
 });
 
-router.get('/:user/hives', (req, res) => {
-    res.send(`Listing hives of ${req.params.user}`)
+router.get('/logout', (req, res) => {
+    req.session = null;
+    res.redirect('/?message=loggedOut');
+});
+
+router.get('/addhive', (req, res) => {
+    if (req.session.username == null) {
+        res.redirect('/login?reason=noSession&redirect=addhive');
+    } else {
+        res.send('TODO: add new hive form');
+    }
+});
+
+router.get('/hives', (req, res) => {
+    if (req.session.username == null) {
+        res.redirect('/login?reason=noSession&redirect=hives');
+    } else {
+        res.send('TODO: add hives list');
+    }
 });
 
 // FAKE SESSION SETTINGS
-router.get('/session/set/:user', (req, res) => {
-    if (req.params.user) {
-        req.session.name = req.params.user;
-        res.send(`Set user session to ${req.params.user}`);
+router.get('/session/set/:username', (req, res) => {
+    if (req.params.username) {
+        req.session.username = req.params.username;
+        res.send(`Set user session to ${req.params.username}`);
     } else {
         res.send('No user given.');
     }
 });
 
 router.get('/session/get', (req, res) => {
-    if (req.session.name) {
-        res.send(req.session.name);
+    if (req.session.username) {
+        res.send(req.session.username);
     } else {
         res.send('No session.');
     }
